5cbb502d206055416eb060340201fcf1d9a1e7b6
Nyarll <oss314y08@i.softbank.jp>
2025-06-21 17:22:26 +0900

    update

--- FruitIdle/Assets/Scripts/CrimeManager.cs
+++ FruitIdle/Assets/Scripts/CrimeManager.cs
@@ -15,26 +15,58 @@ public class NewBehaviourScript : MonoBehaviour
     private List<Dimension> dimensions = new();
     private List<TextMeshProUGUI> dimensionTexts = new();
     private List<Button> dimensionButtons = new();
+    private Color[] dimensionColors = new Color[]
+    {
+        new Color(1.0f, 0f, 0f, 1.0f),
+        new Color(1.0f, 0.647f, 0f, 1.0f),
+        new Color(1.0f, 1.0f, 0f, 1.0f),
+        new Color(0.486f, 0.988f, 0f, 1.0f),
+        new Color(0f, 0.98f, 0.603f, 1.0f),
+        new Color(0f, 1.0f, 1.0f, 1.0f),
+        new Color(0f, 0f, 0.803f, 1.0f),
+        new Color(0.58f, 0f, 0.827f, 1.0f),
+        new Color(1.0f, 0f, 1.0f, 1.0f),
+        new Color(1.0f, 1.0f, 1.0f, 1.0f)
+    };
+
+    void init()
+    {
+        var firstDim = new Dimension("1st Dimension", 1, 10, 1);
+        firstDim.amount = 1;
+        dimensions.Add(firstDim);
+        dimensions.Add(new Dimension("2nd Dimension", 2, 100, 1));
+        dimensions.Add(new Dimension("3rd Dimension", 3, 1000, 1));
+        dimensions.Add(new Dimension("4th Dimension", 4, 10000, 1));
+        dimensions.Add(new Dimension("5th Dimension", 4, 100000, 1));
+        dimensions.Add(new Dimension("6th Dimension", 4, 1000000, 1));
+        dimensions.Add(new Dimension("7th Dimension", 4, 10000000, 1));
+        dimensions.Add(new Dimension("8th Dimension", 4, 100000000, 1));
+        dimensions.Add(new Dimension("9th Dimension", 4, 1000000000, 1));
+        dimensions.Add(new Dimension("10th Dimension", 4, 10000000000, 1));
+    }
 
     void Start()
     {
-        var firstDimension = new Dimension("1st Dimension", 1, 10, 1);
-        dimensions.Add(firstDimension);
-        dimensions.Add(new Dimension("2nd Dimension", 2, 100, 1));
-        dimensions.Add(new Dimension("3rd Dimension", 3, 1000, 1));
-
-        foreach (Dimension dim in dimensions)
+        init();
+        for (int i = 0; i < dimensions.Count; i++)
         {
             GameObject ui = Instantiate(dimensionUIPrefab, dimensionPanelParent);
+
+            Image background = ui.transform.Find("Panel").GetComponent<Image>();
+            if (background != null && i < dimensionColors.Length)
+            {
+                background.color = dimensionColors[i];
+            }
+
             var text = ui.transform.Find("Text").GetComponent<TextMeshProUGUI>();
             var button = ui.transform.Find("Button").GetComponent<Button>();
             dimensionTexts.Add(text);
             dimensionButtons.Add(button);
-            dim.UpdateCost();
 
-            button.onClick.AddListener(() => BuyDimension(dim));
+            int index = i;
+            dimensions[index].UpdateCost();
+            button.onClick.AddListener(() => BuyDimension(dimensions[index]));
         }
-        firstDimension.amount += 1;
     }
 
     void Update()
@@ -62,11 +94,23 @@ public class NewBehaviourScript : MonoBehaviour
         }
 
         // 表示更新
-        crimeText.text = $"{crime:F2}";
+        crimeText.text = $"{FormatNumber(crime)}";
 
         for (int i = 0; i < dimensions.Count; i++)
         {
-            dimensionTexts[i].text = $"{dimensions[i].name}: {dimensions[i].amount}\nCost: {dimensions[i].cost:F2}";
+            dimensionTexts[i].text = $"{dimensions[i].name}: {FormatNumber(dimensions[i].amount, 100000)}\nCost: {FormatNumber(dimensions[i].cost)}";
+        }
+    }
+
+    string FormatNumber(double value, int threshold = 100000000)
+    {
+        if (value < threshold)
+        {
+            return value.ToString("F2");
+        }
+        else
+        {
+            return value.ToString("E2");
         }
     }
 
